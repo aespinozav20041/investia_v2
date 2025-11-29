0c4739748a9a10235d3047bf59970746aa948267
aespinozav20041 <aespinozav2004@gmail.com>
2025-11-29 06:02:04 +0100

    Delete backend directory

--- backend/app/api/v1/users.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/users", tags=["users"])
-
-
-@router.get("/")
-async def list_users_stub():
-    return {"detail": "User endpoints to be implemented"}
